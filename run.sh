75f7a156d2b0176197fbbbb7e44d8209987067bd
Liuhao Lin <2185349651@qq.com>
2025-05-16 17:50:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export API_BASE_URL="https://ms-xfr6fl5w-100034032793-sw.gw.ap-shanghai.ti.tencentcs.com/ms-xfr6fl5w/v1"
-export API_KEY="sk-c1f46d54b4fe496995074b9f0cd4ba46"
+export API_BASE_URL="" # put your model api url here
+export API_KEY="" # put your model api key here
 
-python run_test.py --model_id Qwen2.5-72B-INT8
+python run_test.py --model_id QwQ-32B

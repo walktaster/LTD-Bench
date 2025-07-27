2f7f06c1f9195912aa753a0da4bec357909f1c07
Liuhao Lin <2185349651@qq.com>
2025-07-27 22:06:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export API_BASE_URL="" # put your model api url here
 export API_KEY="" # put your model api key here
 
-python run_test.py --model_id QwQ-32B
+python run_test.py --model_id QwQ-32B  # "model_id" is the name of the model you want to run

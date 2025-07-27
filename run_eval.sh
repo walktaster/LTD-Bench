e13c1dd5e520eacb4f126549bda9ac0e8c9bde15
Liuhao Lin <2185349651@qq.com>
2025-07-27 22:06:23 +0800

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,4 +1,4 @@
 export OPENAI_BASE_URL="" # Put your openai api url here
 export OPENAI_API_KEY="" # Put your openai api key here
 
-python run_eval.py --model_id QwQ-32B --eval_rounds 5
+python run_eval.py --model_id QwQ-32B --eval_rounds 5  # "model_id" is the name of the model you want to evaluate
